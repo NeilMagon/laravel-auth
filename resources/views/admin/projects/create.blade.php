c0c262706277a5b8999cba0b7f6031b2ffb591af
NeilMagon <neil.magon97@gmail.com>
2024-06-04 15:08:34 +0200

    feat: add validation

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -2,19 +2,30 @@
 
 @section('content')
     <h1>Crea un nuovo progetto</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="{{ route('admin.projects.store') }}" method="POST">
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="name" name="name">
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
         </div>
         <div class="mb-3">
             <label for="client_name" class="form-label">Nome cliente</label>
-            <input type="text" class="form-control" id="client_name" name="client_name">
+            <input type="text" class="form-control" id="client_name" name="client_name" value="{{ old('client_name') }}">
         </div>
         <div class="mb-3">
             <label for="summary" class="form-label">Descrizione</label>
-            <textarea class="form-control" id="summary" rows="10" name="summary"></textarea>
+            <textarea class="form-control" id="summary" rows="10" name="summary">{{ old('summary') }}</textarea>
           </div>
         <button type="submit" class="btn btn-primary">Salva</button>
     </form>
